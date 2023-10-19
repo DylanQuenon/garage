b493f60966e3de1679e7d042bfb7ac6152bf41fc
DylanQuenon <dylan.quenon.04@gmail.com>
2023-10-19 17:14:32 +0200

    connexion, inscription + applicationtype

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\Exception\TooManyLoginAttemptsAuthenticationException;
+
+class AccountController extends AbstractController
+{
+    #[Route('/login', name: 'account_login')]
+    public function index(AuthenticationUtils $utils): Response
+    {
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+
+        $loginError = null;
+        if($error instanceof TooManyLoginAttemptsAuthenticationException)
+        {
+            // l'ereur est due à la limitation de tentative de connexion
+            $loginError = "Trop de tentatives de connexion. Réessayez plus tard";
+        }
+
+        return $this->render('account/index.html.twig', [
+            'hasError' => $error !== null,
+            'username' => $username,
+            'loginError' => $loginError
+        
+        ]);
+    }
+    #[Route("/logout", name: "account_logout")]
+    public function logout(): void
+    {
+
+    }
+     /**
+     * Permet d'afficher le formulaire d'inscription ainsi la gestion de l'inscription de l'utilisateur
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @param UserPasswordHasherInterface $hasher
+     * @return Response
+     */
+    #[Route("/register", name:"account_register")]
+    public function register(Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $hasher): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+
+        // partie traitement du formulaire
+        if($form->isSubmitted() && $form->isValid())
+        {
+            // gestion de l'inscription dans la bdd
+            $hash = $hasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($hash);
+
+            $manager->persist($user);
+            $manager->flush();
+
+
+            return $this->redirectToRoute('account_login');
+        }
+
+
+        return $this->render("account/registration.html.twig",[
+            'myForm' => $form->createView()
+        ]);
+    }
+
+
+}
